4f1ca2a17f38a89cb0cd81d3de65f5857d580322
NaufalRafif <77334971+ThePushi@users.noreply.github.com>
2021-04-03 15:50:39 +0700

    Add files via upload

--- donasi/index.php
+++ donasi/index.php
@@ -31,19 +31,19 @@
 <body>
 <header>
 <nav class="navbar navbar-expand-lg navbar-dark back-black">
-  <div class="container">
-    <a class="navbar-brand" href="#"><img src="../DEWATA.png" height="30" alt="dewatarp" loading="lazy"></a>
-    <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-      <i class="fas fa-bars"></i>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0" style="text-align: center;">
-        <li class="nav-item popo">
-          <a class="nav-link" href="../">BERANDA</a>
-        </li>
-      </ul>
-    </div>
-  </div>
+<div class="container">
+<a class="navbar-brand" href="#"><img src="../DEWATA.png" height="30" alt="dewatarp" loading="lazy"></a>
+<button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+<i class="fas fa-bars"></i>
+</button>
+<div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+<ul class="navbar-nav me-auto mb-2 mb-lg-0" style="text-align: center;">
+<li class="nav-item popo">
+<a class="nav-link" href="../">BERANDA</a>
+</li>
+</ul>
+</div>
+</div>
 </nav>
 </header>
 <div class="container">
